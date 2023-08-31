15e76dce8c17aa1cecda0d1ce5146734e23a00fc
ALABI ANUOLUWAPO OLAMIDE <alabianuoluwapoolamide@ALABIs-MacBook-Pro.local>
2023-08-31 22:33:36 +0100

    To repeat payload 10000 times

--- components/gift-cards/giftCard.js
+++ components/gift-cards/giftCard.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // Get all Gift Card
 router.get('/compressed', (req, res, next) => {
   const payload = 'Faster apps use lesser bandwidth too!!! ';
-  res.send(`${payload.repeat(1000000)}`);
+  res.send(`${payload.repeat(10000)}`);
 });
 router.get('/gift-cards', getGiftCards);
 router.get('/product/:giftCardId', getGiftCard);

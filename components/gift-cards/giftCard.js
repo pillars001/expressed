763e2594c3c98d6e5579709fa77ff36d82538db6
ALABI ANUOLUWAPO OLAMIDE <alabianuoluwapoolamide@ALABIs-MacBook-Pro.local>
2023-08-31 12:15:59 +0100

    first contributions

--- components/gift-cards/giftCard.js
+++ components/gift-cards/giftCard.js
@@ -1,12 +1,12 @@
-import express from 'express'
-import {getGiftCards, getGiftCard} from "./giftCardController";
+import express from 'express';
+import { getGiftCards, getGiftCard } from './giftCardController';
 
 const router = express.Router();
-
+//cloned
 // Get all Gift Card
 router.get('/compressed', (req, res, next) => {
-  const payload = 'Faster apps use lesser bandwidth too!!! '
-  res.send(`${payload.repeat(1000000)}`)
+  const payload = 'Faster apps use lesser bandwidth too!!! ';
+  res.send(`${payload.repeat(1000000)}`);
 });
 router.get('/gift-cards', getGiftCards);
 router.get('/product/:giftCardId', getGiftCard);
